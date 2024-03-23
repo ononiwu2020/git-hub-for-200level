7536ee4fe5e4975ca1d7a477c82e7c31167d5329
Ajaero Chinedum <ajaerochinedunahumbrazil14@gmail.com>
2024-03-23 11:19:24 +0100

    First commit

---
+++ src/MarvelData.js
@@ -0,0 +1,182 @@
+export const characters = [
+  // Jojo Bizzare Adventure
+  {
+    name: "Hermit Purple",
+    alias: "Jonathan joestar",
+    img1: "https://i.pinimg.com/564x/99/86/cb/9986cb6417f8e340d48bd371e42f52e7.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Stand user",
+      "Ripple user",
+      "Hamon user",
+    ],
+    team: "Phantom blood",
+    description:
+      "I strive to be a true gentleman! There are times when a gentleman has to be courageous and fight, even when his opponent is bigger than he is and he knows he's going to losen",
+  },
+  {
+    name: "The spawn of the JOJO family",
+    alias: "Dio Brando",
+    abilities: [
+      "Advanced regeneration",
+       "Stand user",
+        "Vampire"],
+    team: "phnatom blood",
+    img1:  "https://i.pinimg.com/564x/9d/1a/71/9d1a7108c0e53fff5077c99898a997f8.jpg",
+    homeworld: "Earth",
+    description:
+      "Originally a man of low birth but peerless ambition, Dio is a cunning villain of little conscience and has an insatiable hunger for power.",
+  },
+  {
+    name: "Caped baldy",
+    alias: "one punch man",
+    abilities: [
+      "Speed",
+     "Power",
+     "Time travel" 
+    ],
+    team: "Hero Association",
+    img1: "https://i.pinimg.com/564x/17/2e/36/172e36bc68aa39fdc1bc5b95cc8f8bce.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Saitama faces a self-imposed existential crisis, as he is now too powerful to gain any thrill from battle.",
+  },
+  {
+    name: "Garou",
+    alias: "Hero Hunter",
+    abilities: [
+      "Superhuman Physical Prowess", 
+    "Supernatural Senses",
+    "Technique Mimicry"],
+    team: "Monster Association",
+    img1: "https://i.pinimg.com/564x/49/1a/81/491a8195b664f0be81851840c248226d.jpg",
+    homeworld: "Earth",
+
+    description:
+      "The popular will win, the hated will lose. It's such a tragedy. Then I won't lose to anyone. I will become the strongest monster ever and change this scenario.",
+  },
+
+  // Jujutsu Kaisen
+  {
+    name: "The strongest sorcorer of today",
+    alias: "Gojo Satoru",
+    abilities: [
+      "Limitless", 
+    "The six eyes",
+    "Domain expansion user (Unlimited void)"
+  ],
+    team: "Tokyo Metropolitan Curse Technical College",
+    img1: "https://i.pinimg.com/564x/54/11/bd/5411bd40e99f3ea2f4ea05fafaeb4450.jpg",
+    homeworld: "Earth",
+    img3: "https://i.pinimg.com/564x/20/f7/fb/20f7fb6108073ee29cc5f5f0c050930a.jpg",
+    description:
+      "He is a special grade jujutsu sorcerer and widely recognized as the strongest in the world. Satoru is the pride of the Gojo Clan, the first person to inherit both the Limitless and the Six Eyes in four hundred years. He works as a teacher at the Tokyo Jujutsu High and uses his influence to protect and train strong young allies.",
+  },
+
+  {
+    name: "The strongest sorcorer in history",
+    alias: "Ryomen Sukuna",
+    abilities: [
+      "Technique copying",
+       "Dismantle and Cleave",
+        "Domain expansion user(Malevolent Shrine)"
+      ],
+    team: "Unknown",
+    img1: "https://i.pinimg.com/564x/dc/4a/80/dc4a80ba1a0632f9cce2c8e223214d9e.jpg",
+    homeworld: "earth",
+    description:
+      "Ryomen Sukuna, more often referred to as simply Sukuna, is the strongest jujutsu sorcerer from a thousand years ago. Regarded as the undisputed King of Curses, Sukuna is one of the primary antagonists of the Jujutsu Kaisen series.",
+  },
+
+  {
+    name: "The savior of soul society",
+    alias: "Ichigo Kurosaki",
+    abilities: [
+      "soul reaper",
+       "hallow",
+        "quincy",
+        "Fullbringer",
+        "Zanpakuto user"
+      ],
+    team: "Soul society",
+    img1: "https://i.pinimg.com/564x/54/3d/61/543d619efe0623d08097948b183c5e86.jpg",
+    homeworld: "Earth",
+    description:
+      "Ichigo Kurosaki  is a Human who is also a Substitute Shinigami. Ichigo is the son of Isshin and Masaki Kurosaki, and older brother of Karin and Yuzu. He is also married to Orihime Inoue and has a son named Kazui Kurosaki.",
+  },
+  {
+    name: "Traitor of soul society",
+    alias: "Sōsuke Aizen",
+    abilities: [
+      "Hōgyoku",
+       "Master swordsmanship",
+       "Mastery of Kidō",
+       "Zanpakuto user",
+       "Hallow"
+      ],
+    team: "Arrancars",
+    img1: "https://i.pinimg.com/564x/4f/6a/e2/4f6ae29dc47ee95d2153f281bc4eee2b.jpg",
+    homeworld: "Soul society",
+    description:
+      "From the very start, no one has stood upon the heavens. Not you, not I, nor even gods. However, that intolerable vacuum has come to an end. Henceforth...I shall stand upon the heavens.",
+  },
+  {
+    name: "Berserk",
+    alias: "Guts",
+    img1: "https://i.pinimg.com/564x/3b/61/77/3b617770025430c89e76493623886897.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Berserker Armor User",
+      "Superhuman conditioning",
+      "Peak-human conditioning",
+    ],
+    team: "Band of hawk",
+    description:
+      "Even if you force back what was lost, it still won't be the way it was.",
+  },
+  {
+    name: "The white hawk",
+    alias: "Griffith",
+    img1: "https://i.pinimg.com/564x/35/3e/d1/353ed1065c442e66d9d342abe2d3b55d.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Femnito",
+      "Nigh-absoluteness against mortal opposition",
+      "Spatial manipulation"
+    ],
+    team: "The god hand",
+    description:
+      "In life, unrelated to one's social standing or class as determined by man, there are some people who, by nature, are keys that set the world in motion. They are the true elite, as dictated by the golden rule of the universe.",
+  },
+  {
+    name: "Devils footprints",
+    alias: "Shinra Kusakabe",
+    img1: "https://i.pinimg.com/564x/ef/2a/5a/ef2a5a5907a3f4601f7589dbcfed77d3.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Pyrokinesis",
+      "Adolla Burst",
+      "god like abilities"
+    ],
+    team: "Supreme Commanderof the Special Fire Force Company",
+    description:
+      "After his mother's death, and supposedly his younger brother's as well, Shinra joined the Special Fire Force to achieve his goal of becoming a hero, saving people from Spontaneous Human Combustion, and uncovering the truth behind his family's death twelve years ago",
+  },
+  {
+    name: "Knight King",
+    alias: "Arthur Boyle",
+    img1: "https://i.pinimg.com/564x/fe/d1/87/fed187864c3460cab5dc09e3067db6ba.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Pyrokinesis",
+      "Adolla",
+      "Sword king abilities"
+    ],
+    team: "	Fire Soldier (Second Class)",
+    description:
+      "The next morning, he finds a letter from his mom and dad explaining that they left on an adventure to save the world, and that while they are gone, he is king. Arthur, seemingly abandoned, enrolled into the Special Fire Force Training Academy alongside Ogun and Shinra. At some point he would go to a hardware store to get enough fire-resistant carbon to make the hilt of his Excalibur",
+  },
+];
+
+console.log(characters);
